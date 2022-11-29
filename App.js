dc7439f2d5a9dc193b6c39fcfd74d0d33a328484
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2022-11-29 22:23:24 +0500

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import Card from './app/components/Card';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <WelcomeScreen></WelcomeScreen>
+      <ViewImageScreen></ViewImageScreen>
+      
     </View>
   );
 }
