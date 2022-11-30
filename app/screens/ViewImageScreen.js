f7996d87a93f117f35cf9c3afbde8a1510aa0f8f
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2022-11-30 11:47:27 +0500

    Improved and added icons

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,42 +1,43 @@
 import React from "react";
 import { Image, StyleSheet, View } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
 
 import colors from "../config/colors";
 
 function ViewImageScreen(props) {
   return (
     <View style={styles.container}>
-      <View style={styles.closeIcon}>
-        <MaterialCommunityIcons name="close" color="white" size={35} />
-      </View>
-      <View style={styles.deleteIcon}>
-        <MaterialCommunityIcons
-          name="trash-can-outline"
-          color="white"
-          size={35}
-        />
-      </View>
       <Image
         resizeMode="contain"
         style={styles.image}
         source={require("../assets/chair.jpg")}
       />
+      <View style={styles.closeIcon}>
+        <MaterialIcons name="close" size={35} color="white" />
+      </View>
+      <View style={styles.deleteIcon}>
+        <MaterialIcons name="delete" size={35} color="white" />
+      </View>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  closeIcon: {
-    position: "relative",
-  },
   container: {
     backgroundColor: colors.black,
-    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
+  closeIcon: {
+    top: 25,
+    left: 25,
+    position: "absolute",
   },
   deleteIcon: {
-    top: 40,
-    left: 25,
+    top: 25,
+    right: 25,
+    position: "absolute",
   },
   image: {
     width: "100%",
