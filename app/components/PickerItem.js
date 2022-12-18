3dd4aad91fb6ba5ee2ca6109bb1666ced18addfa
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2022-12-18 11:29:16 +0500

    Added Picker Component

---
+++ app/components/PickerItem.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import AppText from './AppText'
+
+export default function PickerItem({label,onPress}) {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <AppText style={styles.text}>{label}</AppText>
+   </TouchableOpacity>
+  )
+}
+const styles = StyleSheet.create({
+  text: {
+    padding: 15,
+  },
+})
