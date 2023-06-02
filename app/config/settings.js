7c1e59fb7a384be1d21164522208b7f0edd2ca2d
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2023-06-02 22:50:36 +0500

    added Backend Folder

--- app/config/settings.js
+++ app/config/settings.js
@@ -2,13 +2,13 @@ import Constants from "expo-constants"
 
 const settings = {
     dev: {
-        apiUrl: "http://192.168.0.101:5000/api",
+        apiUrl: "http://192.168.1.106:9000/api",
     },
     staging: {
-        apiUrl: "http://192.168.0.101:5000/api",
+        apiUrl: "http://192.168.1.106:9000/api",
     },
     prod: {
-        apiUrl: "http://192.168.0.101:5000/api",
+        apiUrl: "http://192.168.1.106:9000/api",
     },
 }
 
